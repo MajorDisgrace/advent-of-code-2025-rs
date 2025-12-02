d128b9ca4dc9ae856dd7a385c4fb6dc71449b88d
Felix <65034189+MajorDisgrace@users.noreply.github.com>
2025-12-02 17:03:38 +0100

    initial commit of day 2

---
+++ day-02/src/solution.rs
@@ -0,0 +1,24 @@
+pub fn solve_part_1(input: &str) -> i32 {
+    0
+}
+
+pub fn solve_part_2(input: &str) -> i32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(solve_part_1(TEST_INPUT), 0);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(solve_part_2(TEST_INPUT), 0);
+    }
+}
