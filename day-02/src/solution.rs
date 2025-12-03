84b602882be86010c462d96adf6d47070249cc17
Felix <65034189+MajorDisgrace@users.noreply.github.com>
2025-12-03 10:31:45 +0100

    first attempt at part 1 day 2

--- day-02/src/solution.rs
+++ day-02/src/solution.rs
@@ -1,5 +1,33 @@
-pub fn solve_part_1(input: &str) -> i32 {
-    0
+pub fn solve_part_1(input: &str) -> i64 {
+    return input
+        .split(',')
+        .map(|s| {
+            let vec: Vec<&str> = s.split('-').collect();
+            let start: i64 = (*vec.get(0).expect("Each range should contain a start!"))
+                .parse()
+                .expect("Input contains Items that are not numbers");
+
+            let end: i64 = (*vec.get(1).expect("Each range should contain an end!"))
+                .parse()
+                .expect("Input contains Items that are not numbers");
+
+            return (start, end);
+        })
+        .flat_map(|t| t.0..=t.1)
+        .filter(|n| {
+            let digits = match n {
+                0 => 1,
+                _ => n.abs().ilog10() as u32 + 1,
+            };
+            // println!("{}", digits);
+            let pow_of_10 = (10 as i64).pow(digits / 2);
+            return match digits % 2 {
+                1 => false,
+                0 => n / pow_of_10 == n - (n / pow_of_10) * pow_of_10,
+                _ => panic!("Something is very wrong!"),
+            };
+        })
+        .sum();
 }
 
 pub fn solve_part_2(input: &str) -> i32 {
@@ -14,7 +42,7 @@ mod tests {
 
     #[test]
     fn test_part_1() {
-        assert_eq!(solve_part_1(TEST_INPUT), 0);
+        assert_eq!(solve_part_1(TEST_INPUT), 1227775554);
     }
 
     #[test]
