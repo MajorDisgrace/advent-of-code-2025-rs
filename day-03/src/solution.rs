9bccaf2a4db61f275cde89881058a3f1d29fdb0e
Felix <65034189+MajorDisgrace@users.noreply.github.com>
2025-12-03 14:26:17 +0100

    Initial commit for day 3

---
+++ day-03/src/solution.rs
@@ -0,0 +1,24 @@
+pub fn solve_part_1(input: &str) -> i32 {
+    0
+}
+
+pub fn solve_part_2(input: &str) -> i32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = "";
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(solve_part_1(TEST_INPUT), 0);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(solve_part_2(TEST_INPUT), 0);
+    }
+}
