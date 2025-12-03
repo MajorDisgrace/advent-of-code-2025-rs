8df37855dc682c15050ca37ad7d20948a7585e53
Felix <65034189+MajorDisgrace@users.noreply.github.com>
2025-12-03 22:24:40 +0100

    finished part 1 of day 3

--- day-03/src/solution.rs
+++ day-03/src/solution.rs
@@ -1,23 +1,34 @@
 pub fn solve_part_1(input: &str) -> i32 {
     let banks = input.trim().split('\n');
 
+    let mut count = 0;
     for bank in banks {
-        let batteries: Vec<u8> = bank
+        let batteries: Vec<usize> = bank
             .chars()
             .map(|c| {
                 c.to_digit(10)
-                    .expect("There are non Number values in the Input!") as u8
+                    .expect("There are non Number values in the Input!") as usize
             })
             .collect();
 
-        let mut first_battery = batteries[0];
+        let mut first_index = 0;
         for i in 1..(batteries.len() - 1) {
-            if first_battery < batteries[i] {
-                first_battery = batteries[i];
+            if batteries[first_index] < batteries[i] {
+                first_index = i;
             }
         }
+
+        let mut second_index = first_index + 1;
+        for i in (second_index + 1)..batteries.len() {
+            if batteries[second_index] < batteries[i] {
+                second_index = i;
+            }
+        }
+
+        count += batteries[first_index] * 10 + batteries[second_index];
     }
-    0
+
+    return count as i32;
 }
 
 pub fn solve_part_2(input: &str) -> i32 {
