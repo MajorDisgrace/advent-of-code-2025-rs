bb2f8a3a67e9ce6e7d2dbdec97268fe124d8a0a3
Felix <65034189+MajorDisgrace@users.noreply.github.com>
2025-12-04 11:37:58 +0100

    solved part 1 of day 4

--- day-04/src/solution.rs
+++ day-04/src/solution.rs
@@ -1,14 +1,102 @@
+struct Matrix {
+    data: Vec<u8>,
+    cols: usize,
+}
+
+impl Matrix {
+    fn get(&self, row: usize, col: usize) -> Option<u8> {
+        if col >= self.cols() {
+            return None;
+        }
+        if row >= self.rows() {
+            return None;
+        }
+
+        let result = *self.data.get(row * self.cols + col)?;
+        return Some(result);
+    }
+
+    fn rows(&self) -> usize {
+        self.data.len() / self.cols()
+    }
+
+    fn cols(&self) -> usize {
+        self.cols
+    }
+
+    // fn push(&mut self, val: u8) {
+    //     self.data.push(val);
+    // }
+}
+
 pub fn solve_part_1(input: &str) -> i32 {
     let input = input.trim();
+    let matrix = str_to_matrix(input);
 
-    0
+    let mut count = 0;
+    for row in 0..matrix.rows() {
+        for col in 0..matrix.cols() {
+            if matrix.get(row, col) == Some(1)
+                && count_adj_paper(&matrix, row as isize, col as isize) < 4
+            {
+                count += 1;
+            }
+        }
+    }
+
+    return count;
 }
 
 pub fn solve_part_2(input: &str) -> i32 {
     0
 }
 
-fn str_to_vec_matrix(s: &str) -> Vec<Vec<u8>> {}
+fn count_adj_paper(matrix: &Matrix, row: isize, col: isize) -> u8 {
+    let mut count = 0;
+
+    for r in (row - 1)..=(row + 1) {
+        for c in (col - 1)..=(col + 1) {
+            if c == col && r == row {
+                //only want adjacent cells
+                continue;
+            }
+            if c < 0 || r < 0 {
+                continue;
+            }
+
+            count += match matrix.get(r as usize, c as usize) {
+                Some(n) => n,
+                None => 0,
+            }
+        }
+    }
+
+    return count;
+}
+
+fn str_to_matrix(s: &str) -> Matrix {
+    let cols = s
+        .lines()
+        .next()
+        .expect("Input should contain at least one line!")
+        .chars()
+        .count();
+
+    let data: Vec<u8> = s
+        .lines()
+        .flat_map(|l| l.chars())
+        .map(|c| match c {
+            '@' => 1 as u8,
+            '.' => 0 as u8,
+            _ => panic!("The input should only contain chars of type '@' and '.'"),
+        })
+        .collect();
+
+    return Matrix {
+        data: data,
+        cols: cols,
+    };
+}
 
 #[cfg(test)]
 mod tests {
@@ -28,7 +116,7 @@ mod tests {
 
     #[test]
     fn test_part_1() {
-        assert_eq!(solve_part_1(TEST_INPUT), 0);
+        assert_eq!(solve_part_1(TEST_INPUT), 13);
     }
 
     #[test]
