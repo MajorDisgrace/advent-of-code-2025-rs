5a14c6e7c5dc8f8ffcb635489c6023634edab607
Felix <65034189+MajorDisgrace@users.noreply.github.com>
2025-12-04 14:53:03 +0100

    solved part 2 of day 4

--- day-04/src/solution.rs
+++ day-04/src/solution.rs
@@ -24,9 +24,17 @@ impl Matrix {
         self.cols
     }
 
-    // fn push(&mut self, val: u8) {
-    //     self.data.push(val);
-    // }
+    fn get_mut(&mut self, row: usize, col: usize) -> Option<&mut u8> {
+        if col >= self.cols() {
+            return None;
+        }
+        if row >= self.rows() {
+            return None;
+        }
+
+        let result = self.data.get_mut(row * self.cols + col)?;
+        return Some(result);
+    }
 }
 
 pub fn solve_part_1(input: &str) -> i32 {
@@ -48,7 +56,29 @@ pub fn solve_part_1(input: &str) -> i32 {
 }
 
 pub fn solve_part_2(input: &str) -> i32 {
-    0
+    let input = input.trim();
+    let mut matrix = str_to_matrix(input);
+
+    let mut count = 0;
+    let mut last_count = -1;
+    while count != last_count {
+        last_count = count;
+        for row in 0..matrix.rows() {
+            for col in 0..matrix.cols() {
+                if matrix.get(row, col) == Some(1)
+                    && count_adj_paper(&matrix, row as isize, col as isize) < 4
+                {
+                    let val = matrix
+                        .get_mut(row, col)
+                        .expect("Something went very wrong!");
+                    *val = 0;
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    return count;
 }
 
 fn count_adj_paper(matrix: &Matrix, row: isize, col: isize) -> u8 {
@@ -121,6 +151,6 @@ mod tests {
 
     #[test]
     fn test_part_2() {
-        assert_eq!(solve_part_2(TEST_INPUT), 0);
+        assert_eq!(solve_part_2(TEST_INPUT), 43);
     }
 }
