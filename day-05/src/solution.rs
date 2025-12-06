2327cd3697ceda6f18b4a1a5274614010fec7f45
Felix <65034189+MajorDisgrace@users.noreply.github.com>
2025-12-06 17:19:18 +0100

    solved part 2 of day 5

--- day-05/src/solution.rs
+++ day-05/src/solution.rs
@@ -37,8 +37,61 @@ pub fn solve_part_1(input: &str) -> usize {
         .count();
 }
 
-pub fn solve_part_2(input: &str) -> i32 {
-    0
+pub fn solve_part_2(input: &str) -> usize {
+    let mut ranges = input
+        .trim()
+        .split("\n\n")
+        .next()
+        .expect("The input should contain a list of ranges and a list of ingredients")
+        .lines()
+        .map(|l| {
+            let mut split = l.split('-');
+
+            return (
+                split
+                    .next()
+                    .expect("Every range should be formated like this: 'start-end'")
+                    .parse::<usize>()
+                    .expect("The start of each range should be an integer"),
+                split
+                    .next()
+                    .expect("Every range should be formated like this: 'start-end'")
+                    .parse::<usize>()
+                    .expect("The end of each range should be an integer"),
+            );
+        })
+        .collect::<Vec<_>>();
+
+    ranges.sort_unstable_by(|(a1, _), (b1, _)| a1.cmp(b1));
+
+    let mut unique_ranges = Vec::new();
+    let mut previous_end = 0;
+    for range in ranges {
+        if unique_ranges.len() == 0 {
+            unique_ranges.push(range);
+            previous_end = range.1;
+            continue;
+        }
+
+        if range.0 <= previous_end && previous_end < range.1 {
+            let t = unique_ranges.pop().expect(
+                "unique_ranges is initialized to contain one tuple and no item is ever removed",
+            );
+            unique_ranges.push((t.0, range.1));
+            previous_end = range.1;
+            continue;
+        } else if range.0 <= previous_end {
+            continue;
+        }
+
+        unique_ranges.push(range);
+        previous_end = range.1;
+    }
+
+    return unique_ranges
+        .iter()
+        .map(|(start, end)| end - start + 1)
+        .sum();
 }
 
 #[cfg(test)]
@@ -66,6 +119,6 @@ mod tests {
 
     #[test]
     fn test_part_2() {
-        assert_eq!(solve_part_2(TEST_INPUT), 0);
+        assert_eq!(solve_part_2(TEST_INPUT), 14);
     }
 }
