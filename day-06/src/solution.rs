c9dec9b127c2c1233ee738ad66348445c9c8d61e
Felix <65034189+MajorDisgrace@users.noreply.github.com>
2025-12-06 19:45:30 +0100

    solved part 1 of day 6

--- day-06/src/solution.rs
+++ day-06/src/solution.rs
@@ -1,5 +1,62 @@
+enum Opp {
+    Add,
+    Mul,
+}
+
 pub fn solve_part_1(input: &str) -> usize {
-    0
+    let input = input.trim();
+
+    // reverse the Iterator for easy acces to the Operators
+    let mut lines = input.lines().rev();
+
+    let opps = lines
+        .next()
+        .expect("Input should contain at least two lines")
+        .split(' ')
+        .filter(|s| !s.is_empty())
+        .map(|s| match s {
+            "+" => Opp::Add,
+            "*" => Opp::Mul,
+            _ => panic!("The last line should only contain spaces '+' and '*'"),
+        })
+        .collect::<Vec<_>>();
+
+    let mut num_lines = Vec::new();
+    for line in lines {
+        num_lines.push(
+            line.split(' ')
+                .filter(|s| !s.is_empty())
+                .map(|s| {
+                    s.parse::<usize>()
+                        .expect("The number lines should only contain spaces and numbers")
+                })
+                .collect::<Vec<_>>(),
+        );
+    }
+    let num_lines = num_lines;
+
+    let mut count = 0;
+    for i in 0..opps.len() {
+        let opp = opps.get(i).expect("i can not be greater than opps.len()");
+        let mut equation_result = match opp {
+            Opp::Add => 0,
+            Opp::Mul => 1,
+        };
+
+        for l in &num_lines {
+            let num = l
+                .get(i)
+                .expect("there should be as many numbers in each line as there are operators");
+
+            match opp {
+                Opp::Add => equation_result += num,
+                Opp::Mul => equation_result *= num,
+            }
+        }
+        count += equation_result;
+    }
+
+    return count;
 }
 
 pub fn solve_part_2(input: &str) -> usize {
@@ -18,7 +75,7 @@ mod tests {
 
     #[test]
     fn test_part_1() {
-        assert_eq!(solve_part_1(TEST_INPUT), 0);
+        assert_eq!(solve_part_1(TEST_INPUT), 4277556);
     }
 
     #[test]
