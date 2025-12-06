6e178e1e92c420d0913de28c0daf9734f7e249a1
Felix <65034189+MajorDisgrace@users.noreply.github.com>
2025-12-06 09:55:27 +0100

    solved part 1 of day 5

--- day-05/src/solution.rs
+++ day-05/src/solution.rs
@@ -1,5 +1,40 @@
-pub fn solve_part_1(input: &str) -> i32 {
-    0
+pub fn solve_part_1(input: &str) -> usize {
+    let input = input.trim();
+
+    let mut split_input = input.split("\n\n");
+
+    let ranges: Vec<(usize, usize)> = split_input
+        .next()
+        .expect("The input should contain a list of ranges and a list of ingredients")
+        .lines()
+        .map(|l| {
+            let mut split = l.split('-');
+
+            return (
+                split
+                    .next()
+                    .expect("Every range should be formated like this: 'start-end'")
+                    .parse()
+                    .expect("The start of each range should be an integer"),
+                split
+                    .next()
+                    .expect("Every range should be formated like this: 'start-end'")
+                    .parse()
+                    .expect("The end of each range should be an integer"),
+            );
+        })
+        .collect();
+
+    return split_input
+        .next()
+        .expect("The input shoulc contain a list of available ingredient IDs")
+        .lines()
+        .map(|l| {
+            l.parse::<usize>()
+                .expect("The available ingredient IDs should exclusively be numbers!")
+        })
+        .filter(|n| ranges.iter().any(|t| t.0 <= *n && *n <= t.1))
+        .count();
 }
 
 pub fn solve_part_2(input: &str) -> i32 {
@@ -26,7 +61,7 @@ mod tests {
 
     #[test]
     fn test_part_1() {
-        assert_eq!(solve_part_1(TEST_INPUT), 0);
+        assert_eq!(solve_part_1(TEST_INPUT), 3);
     }
 
     #[test]
