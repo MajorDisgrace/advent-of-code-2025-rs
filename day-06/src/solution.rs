8c2e7bf52a7377c6b3c196ca1fab464d465cb9dc
Felix <65034189+MajorDisgrace@users.noreply.github.com>
2025-12-07 11:01:53 +0100

    solved part 2 of day 6

--- day-06/src/solution.rs
+++ day-06/src/solution.rs
@@ -60,7 +60,68 @@ pub fn solve_part_1(input: &str) -> usize {
 }
 
 pub fn solve_part_2(input: &str) -> usize {
-    0
+    let input = input.trim_matches('\n');
+
+    // reverse the Iterator for easy acces to the Operators
+    let mut lines = input.lines().rev();
+
+    let opps = lines
+        .next()
+        .expect("Input should contain at least two lines")
+        .split(' ')
+        .filter(|s| !s.is_empty())
+        .map(|s| match s {
+            "+" => Opp::Add,
+            "*" => Opp::Mul,
+            _ => panic!("The last line should only contain spaces '+' and '*'"),
+        })
+        .collect::<Vec<_>>();
+
+    let mut num_lines = Vec::new();
+    for line in lines {
+        num_lines.push(line.chars().map(|c| c.to_digit(10)).collect::<Vec<_>>());
+    }
+    let num_lines = num_lines;
+
+    let mut count = 0;
+    let mut i = 0;
+    for opp in opps {
+        let mut equation_result = match opp {
+            Opp::Add => 0,
+            Opp::Mul => 1,
+        };
+
+        const TEN: usize = 10;
+        loop {
+            let mut num = 0;
+            let mut j = 0;
+            for l in &num_lines {
+                let digit = match l.get(i) {
+                    Some(a) => a,
+                    None => &None,
+                };
+
+                if *digit != None {
+                    num += (digit.unwrap() as usize) * TEN.pow(j);
+                    j += 1;
+                }
+            }
+
+            i += 1;
+            if num == 0 {
+                break;
+            }
+
+            match opp {
+                Opp::Add => equation_result += num,
+                Opp::Mul => equation_result *= num,
+            }
+        }
+
+        count += equation_result;
+    }
+
+    return count;
 }
 
 #[cfg(test)]
